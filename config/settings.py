af56312032da8a47a61ca0d2db9490ab84581b38
nishi <nishimura.bruno@gmail.com>
2022-04-20 15:50:23 -0300

    small fix

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['protected-citadel-70188.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -152,3 +152,4 @@ if ENVIRONMENT == 'production':
     SECURE_CONTENT_TYPE_NOSNIFF = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
